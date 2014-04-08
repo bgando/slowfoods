aeb616481ed4a5fe5f83382ff651a3fba8a798d2
Bianca <gando.bia@gmail.com>
2014-04-07 17:11:47 -0700

    playing

--- js/app.js
+++ js/app.js
@@ -17,3 +17,61 @@ $(document).on( "mobileinit", function() {
     $.mobile.pushStateEnabled = false;
     $.mobile.defaultPageTransition = "none";
 });
+//{ "geo": { "$nearSphere": {"__type": "GeoPoint", "latitude": 30.0, "longitude": -20.0}}}
+
+$('body').on('click', '#search-btn', function(e){
+  e.preventDefault();
+  var query = encodeURI($('#search-text').val());
+  console.log('query', query);
+
+  
+
+    $('#map').attr('src', 'https://www.google.com/maps/embed/v1/place?key=AIzaSyBZhaRczXtpJHUEnPeq300rP6ji6-X4IEI&q=' + query);
+
+  });
+
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/restaurants/',
+    method: 'GET',
+    dataType: 'json',
+    headers: {
+      'X-Parse-Application-Id': '6W64LqKgGiD8T5bh0ZS7hqipzy3uO4CZXEMHtJeG',
+      'X-Parse-REST-API-Key': 'YNHIPKXWGsIBYyZXrshrZlVto8OyHzMrcHE30QD1'
+    },
+    success: function(data){
+      populateList(data.results);
+    },
+    error: function(err){
+      console.log('err', err)
+    }
+
+});
+
+  var populateList = function(data){
+   console.log('data', data[0].name);
+   var counter = 70;
+   for (var i = 0; i < data.length; i++){
+      // console.log(data[i]);
+      var item = '<div class="list-wrapper">' +
+          '<div class="img">' +
+            '<img class="img" src="http://placekitten.com/80/'+ (counter + i) +'"/>' +
+          '</div>' +
+          '<div class="info">' +
+            '<div class="name-info">' +
+              '<p class="name">' + data[i].name + '</p>' +
+              '<p class="address">' + data[i].address + '</p>' +
+            '</div>' +
+            '<div class="desc">' +
+              '<p class="description">' + data[i].description + '</p>' +
+              '<p class="meta"></p>' +
+            '</div>' +
+          '</div>' +
+        '</div>';
+
+       $('#restuarant-list').append('<li>'+ item + '</li>');
+   }
+
+  }
+
+
+
